21cff9e6dfe70186ed6d9f4809dfe681e47cf88f
Jassinger123 <104770712+Jassinger123@users.noreply.github.com>
2022-11-14 18:50:20 +0530

    Add files via upload

---
+++ music_instrument.js
@@ -0,0 +1,50 @@
+img ="";
+
+status="";
+function preload(){
+    img=loadImage('music_instrument.png');
+}
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    object_detector=ml5.objectDetector('cocossd',modal_loaded)
+}
+
+function modal_loaded(){
+    console.log("modal_loaded")
+    status=true;
+    object_detector.detect(img,got_results)
+    document.getElementById("status").innerHTML="status=Detecting Objects"
+
+}
+
+function got_results(error,results){
+ if (error) {
+    console.log(error);
+ }
+ console.log(results)
+}
+
+
+
+function draw(){
+    image(img,0,0,640,420);
+    fill("blue");
+    text("Voilin", 50, 70);
+    noFill();
+    stroke("red");
+rect(30,60,450,350);
+}
+
+function button_home(){
+    window.location=("index.html");
+}
+
+
+
+
+
+
+
+
